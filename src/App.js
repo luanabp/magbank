5f1818f8f4ac195ad893303787895a879360f02d
luanabp <luanaboninportes@gmail.com>
2021-03-10 01:10:11 -0300

    institutional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Credit from "./components/Credit";
 import CardList from "./components/CardList";
 import CtaButton from "./components/CtaButton";
+import Institutional from "./components/Institutional";
 import "./App.scss";
 
 import posts from "./content/posts";
@@ -15,6 +16,7 @@ const App = () => (
     <Credit />
     <CardList posts={posts} />
     <CtaButton>Abra sua conta</CtaButton>
+    <Institutional />
   </div>
 );
 
