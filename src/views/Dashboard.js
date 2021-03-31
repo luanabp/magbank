489252d39c9145f4eb0109d28f089de199ec529d
luanabp <luanaboninportes@gmail.com>
2021-03-31 02:03:50 -0300

    table

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Tabs, Tab, Table } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faUser } from "@fortawesome/free-solid-svg-icons";
 
@@ -53,8 +53,97 @@ const Dashboard = () => (
           Extrato
         </Button>
       </Col>
-      <Col xs={12} lg={3}></Col>
-      <Col xs={12} lg={5}></Col>
+      <Col xs={12} lg={3} className="mt-lg-5 pt-lg-4">
+        <h3 className="my-5">Conta Corrente</h3>
+        <h6>
+          <small>
+            <strong>Saldo em conta corrente</strong>
+          </small>
+        </h6>
+        <h4 className="text-success mb-4">
+          <strong>
+            <small>R$ </small>3.500<small>,00</small>
+          </strong>
+        </h4>
+        <h6>
+          <small>
+            <strong>Cheque especial</strong>
+          </small>
+        </h6>
+        <p className="mb-0">Limite disponível</p>
+        <p className="mb-4">R$ 5.000,00</p>
+        <Button variant="secondary">Ver extrato</Button>
+      </Col>
+      <Col xs={12} lg={5} className="mt-lg-5 pt-lg-5">
+        <Tabs className="mt-5 pt-lg-5" defaultActiveKey="latest">
+          <Tab eventKey="latest" title="Últimos Lançamentos">
+            <Table striped borderless>
+              <thead>
+                <tr>
+                  <th>Data</th>
+                  <th>Descrição</th>
+                  <th>Valor (R$)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>22/07</td>
+                  <td>SAQUE 24H 012345</td>
+                  <td>300,00</td>
+                </tr>
+                <tr>
+                  <td>21/07</td>
+                  <td>SUPERMERCADO 2312332</td>
+                  <td>275,90</td>
+                </tr>
+                <tr>
+                  <td>21/07</td>
+                  <td>ESTACIONAMENTO 123234</td>
+                  <td>12,00</td>
+                </tr>
+                <tr>
+                  <td>21/07</td>
+                  <td>PAGAMENTO ALUGUEL 123432</td>
+                  <td>1.500,00</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Tab>
+          <Tab eventKey="future" title="Lançamentos Futuros">
+            <Table striped borderless>
+              <thead>
+                <tr>
+                  <th>Data</th>
+                  <th>Descrição</th>
+                  <th>Valor (R$)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>24/07</td>
+                  <td>NETFLIX 012345</td>
+                  <td>32,90</td>
+                </tr>
+                <tr>
+                  <td>24/07</td>
+                  <td>PRIME VIDEO 2312332</td>
+                  <td>9,90</td>
+                </tr>
+                <tr>
+                  <td>23/07</td>
+                  <td>DISNEY+ 123234</td>
+                  <td>27,90</td>
+                </tr>
+                <tr>
+                  <td>22/07</td>
+                  <td>HBO GO 123432</td>
+                  <td>34,90</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Tab>
+        </Tabs>
+      </Col>
     </Row>
   </Container>
 );
